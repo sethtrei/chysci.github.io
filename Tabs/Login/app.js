021f1cff7f0131cfd595b9d7ce881629381e4b75
XenonMolecule <michael_ryan_2000@yahoo.com>
2015-11-03 22:56:13 +0000

    Added a limited functionality login

--- Tabs/Login/app.js
+++ Tabs/Login/app.js
@@ -1,1 +1,17 @@
 // JavaScript File
+myFirebaseRef = new Firebase("https://vivid-torch-4895.firebaseio.com/");
+function authorize(){
+    var username = $('#usernameInput').val();
+    var password = $('#passwordInput').val();
+    newRef = new Firebase("https://vivid-torch-4895.firebaseio.com/users/"+username+"/");
+    newRef.child("password").on("value", function(snapshot){
+        if(password === snapshot.val()){
+            login(username);
+        } else {
+            alert("That username and password combination does not match");
+        }
+    });
+}
+function login(username){
+    alert("You're in "+username+"!");
+}
