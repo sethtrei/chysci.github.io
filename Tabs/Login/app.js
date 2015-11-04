937ad26361a80297a6064c32db1bda470b55a8a9
XenonMolecule <michael_ryan_2000@yahoo.com>
2015-11-04 21:18:46 +0000

    Began adding a dropdown menu hidden in the Signup page, and made the login a little more secure.  Also continued sethtrei's work of cleaning up that pesky font

--- Tabs/Login/app.js
+++ Tabs/Login/app.js
@@ -1,17 +1,37 @@
 // JavaScript File
 myFirebaseRef = new Firebase("https://vivid-torch-4895.firebaseio.com/");
+var theEmail;
 function authorize(){
     var username = $('#usernameInput').val();
     var password = $('#passwordInput').val();
     newRef = new Firebase("https://vivid-torch-4895.firebaseio.com/users/"+username+"/");
-    newRef.child("password").on("value", function(snapshot){
-        if(password === snapshot.val()){
-            login(username);
-        } else {
-            alert("That username and password combination does not match");
-        }
+    newRef.child("email").on("value",function(snapshot){
+        theEmail = snapshot.val();
+        alert(theEmail);
+        fireAuth(username, password, newRef);
     });
 }
 function login(username){
     alert("You're in "+username+"!");
+}
+
+function fireAuth(username, password, newRef){
+    newRef.child("password").on("value", function(snapshot){
+        if(password === snapshot.val()){
+            myFirebaseRef.authWithPassword({
+                email : theEmail,
+                password : snapshot.val()
+            }, function(error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+                alert("Login Failed: "+ error);
+            } else {
+                console.log("Authenticated successfully with payload:", authData);
+                login(username);
+            }
+        });
+        } else {
+            alert("That username and password combination does not match");
+        }
+    });
 }
